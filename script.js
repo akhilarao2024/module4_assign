3e3298d82b743746b1d7af5f19c49d308bebcd00
akhilarao2024 <akhilarao2024@gmail.com>
2018-12-03 21:27:03 -0600

    module 4 assignment

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function() {
-    var names = ["Yaami", "Joana", "Jaan", "Jack", "Peter", "Fry", "Laila", "Parker", "Lassi", "Joe"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
